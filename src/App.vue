bfda4533fbf49e697a8965a5279a55902ea76109
zhuoyuan <zhuoyuan1028@gmail.com>
2018-04-07 21:44:14 +0800

    add front size and blank

--- src/App.vue
+++ src/App.vue
@@ -74,8 +74,8 @@
   .tab
     display: flex
     width: 100%
-    height: 40px
-    line-height: 40px
+    height: 25px
+    line-height: 25px
     // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
     border-1px(rgba(7, 17, 27, 0.1))
     .tab-item
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -23,9 +23,9 @@
                 <div class="content">
                   <h2 class="name">{{food.name}}</h2>
                   <p class="desc">{{food.description}}</p>
-                  <div class="extra">
+                  <!-- <div class="extra">
                     <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
-                  </div>
+                  </div> -->
                   <div class="price">
                     <span class="now">￥{{food.price}}</span><span class="old"
                                                                   v-show="food.oldPrice">￥{{food.oldPrice}}</span>
@@ -206,7 +206,7 @@
   .goods
     display: flex
     position: absolute
-    top: 174px
+    top: 159px
     bottom: 46px
     width: 100%
     overflow: hidden
@@ -216,7 +216,7 @@
       background: #f3f5f7
       .menu-item
         display: table
-        height: 54px
+        height: 34px
         width: 56px
         padding: 0 12px
         line-height: 14px
@@ -228,24 +228,24 @@
           font-weight: 700
           .text
             border-none()
-        .icon
-          display: inline-block
-          vertical-align: top
-          width: 12px
-          height: 12px
-          margin-right: 2px
-          background-size: 12px 12px
-          background-repeat: no-repeat
-          &.decrease
-            bg-image('decrease_3')
-          &.discount
-            bg-image('discount_3')
-          &.guarantee
-            bg-image('guarantee_3')
-          &.invoice
-            bg-image('invoice_3')
-          &.special
-            bg-image('special_3')
+        // .icon
+        //   display: inline-block
+        //   vertical-align: top
+        //   width: 12px
+        //   height: 12px
+        //   margin-right: 2px
+        //   background-size: 12px 12px
+        //   background-repeat: no-repeat
+        //   &.decrease
+        //     bg-image('decrease_3')
+        //   &.discount
+        //     bg-image('discount_3')
+        //   &.guarantee
+        //     bg-image('guarantee_3')
+        //   &.invoice
+        //     bg-image('invoice_3')
+        //   &.special
+        //     bg-image('special_3')
         .text
           display: table-cell
           width: 56px
@@ -264,8 +264,8 @@
         background: #f3f5f7
       .food-item
         display: flex
-        margin: 18px
-        padding-bottom: 18px
+        margin: 9px
+        padding-bottom: 1px
         border-1px(rgba(7, 17, 27, 0.1))
         &:last-child
           border-none()
@@ -277,17 +277,17 @@
           flex: 1
           .name
             margin: 2px 0 8px 0
-            height: 14px
-            line-height: 14px
-            font-size: 14px
+            height: 17px
+            line-height: 17px
+            font-size: 17px
             color: rgb(7, 17, 27)
           .desc, .extra
             line-height: 10px
-            font-size: 10px
+            font-size: 13px
             color: rgb(147, 153, 159)
           .desc
-            line-height: 12px
-            margin-bottom: 8px
+            line-height: 6px
+            margin-bottom: 4px
           .extra
             .count
               margin-right: 12px
